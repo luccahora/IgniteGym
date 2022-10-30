0d5b0d2489bbc15516e77b6f095985b973ac0e6d
Lucca Hora <luccahora@gmail.com>
2022-10-30 15:06:12 -0300

    chore: groups

--- ignitegym/src/screens/Home/index.tsx
+++ ignitegym/src/screens/Home/index.tsx
@@ -1,11 +1,38 @@
-import { Center, Text } from "native-base";
-import React from "react";
+import Group from "@components/Group";
+import HomeHeader from "@components/HomeHeader";
+import { Center, FlatList, HStack, Text, VStack } from "native-base";
+import React, { useState } from "react";
 
 const Home: React.FC = () => {
+  const [groupSelected, setGroupSelected] = useState("");
+  const [groups, setGroups] = useState([
+    "Costas",
+    "Bíceps",
+    "Tríceps",
+    "Ombro",
+  ]);
+
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+    </VStack>
   );
 };
 
