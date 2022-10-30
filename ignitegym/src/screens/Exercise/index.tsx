bf9100fb0e8997aea5279610989440ddc865d0ef
Lucca Hora <luccahora@gmail.com>
2022-10-30 01:28:48 -0300

    chore: create screens

---
+++ ignitegym/src/screens/Exercise/index.tsx
@@ -0,0 +1,12 @@
+import { Center, Text } from "native-base";
+import React from "react";
+
+const Exercise: React.FC = () => {
+  return (
+    <Center flex={1}>
+      <Text>Exercise</Text>
+    </Center>
+  );
+};
+
+export default Exercise;
---
+++ ignitegym/src/screens/History/index.tsx
@@ -0,0 +1,12 @@
+import { Center, Text } from "native-base";
+import React from "react";
+
+const History: React.FC = () => {
+  return (
+    <Center flex={1}>
+      <Text>History</Text>
+    </Center>
+  );
+};
+
+export default History;
