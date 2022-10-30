b3ea64ae7fc0fc22bc0ae6ef08fc69f78d95984d
Lucca Hora <luccahora@gmail.com>
2022-10-30 01:46:26 -0300

    chore: remove header

--- ignitegym/src/routes/app.routes.tsx
+++ ignitegym/src/routes/app.routes.tsx
@@ -20,7 +20,7 @@ const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
       <Screen name="exercise" component={Exercise} />
       <Screen name="profile" component={Profile} />
