1288463ae79fd13e130e7a388958106e91b9a099
Lucca Hora <luccahora@gmail.com>
2022-11-05 18:58:40 -0300

    chore: exercise list

--- ignitegym/src/screens/Home/index.tsx
+++ ignitegym/src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
+import { ExerciseCard } from "@components/ExerciseCard";
 import Group from "@components/Group";
 import HomeHeader from "@components/HomeHeader";
-import { Center, FlatList, HStack, Text, VStack } from "native-base";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 import React, { useState } from "react";
 
 const Home: React.FC = () => {
@@ -10,6 +11,12 @@ const Home: React.FC = () => {
     "Bíceps",
     "Tríceps",
     "Ombro",
+  ]);
+  const [exercises, setExercises] = useState([
+    "Puxada frontal",
+    "Remada curvada",
+    "Remada unilateral",
+    "Levantamento terra",
   ]);
 
   return (
@@ -32,6 +39,23 @@ const Home: React.FC = () => {
         my={10}
         maxH={10}
       />
+      <VStack flex={1} px="8">
+        <HStack justifyContent={"space-between"} mb={"5"}>
+          <Heading color={"gray.200"} fontSize={"md"}>
+            Exercícios
+          </Heading>
+          <Text color={"gray.200"} fontSize={"sm"}>
+            {exercises.length}
+          </Text>
+        </HStack>
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
     </VStack>
   );
 };
