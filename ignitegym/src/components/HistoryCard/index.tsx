df2ef8b6dc6150d8435ead0b3e7eafb419881575
Lucca Hora <luccahora@gmail.com>
2022-11-10 18:21:53 -0300

    chore: history card

---
+++ ignitegym/src/components/HistoryCard/index.tsx
@@ -0,0 +1,36 @@
+import { Heading, HStack, Text, VStack } from "native-base";
+import React from "react";
+
+const HistoryCard: React.FC = () => {
+  return (
+    <HStack
+      w={"full"}
+      px={5}
+      py={4}
+      mb={3}
+      bg="gray.600"
+      rounded={"md"}
+      alignItems="center"
+      justifyContent={"space-between"}
+    >
+      <VStack mr={5}>
+        <Heading
+          color={"white"}
+          fontSize="md"
+          fontFamily={"heading"}
+          textTransform={"capitalize"}
+        >
+          Costas
+        </Heading>
+        <Text color={"gray.100"} fontSize="lg" numberOfLines={1}>
+          Puxada fontral
+        </Text>
+      </VStack>
+      <Text color={"gray.300"} fontSize="md">
+        08:56
+      </Text>
+    </HStack>
+  );
+};
+
+export default HistoryCard;
