00b989aa88660199ea91b5b367bc82790b96c0b5
Lucca Hora <luccahora@gmail.com>
2022-11-26 12:40:20 -0300

    chore: user photo

--- ignitegym/src/screens/Profile/index.tsx
+++ ignitegym/src/screens/Profile/index.tsx
@@ -1,11 +1,21 @@
 import { ScreenHeader } from "@components/ScreenHeader";
-import { Center, Text, VStack } from "native-base";
+import UserPhoto from "@components/UserPhoto";
+import { Center, ScrollView, Text, VStack } from "native-base";
 import React from "react";
 
 const Profile: React.FC = () => {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            source={{ uri: "https://github.com/luccahora.png" }}
+            alt="Foto do usuario"
+            size={33}
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   );
 };
