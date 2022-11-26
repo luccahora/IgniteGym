d61cd58576d3c7362681b7696485ee57a84306fd
Lucca Hora <luccahora@gmail.com>
2022-11-26 12:52:59 -0300

    chore: Skeleton effect

--- ignitegym/src/screens/Profile/index.tsx
+++ ignitegym/src/screens/Profile/index.tsx
@@ -1,19 +1,33 @@
 import { ScreenHeader } from "@components/ScreenHeader";
 import UserPhoto from "@components/UserPhoto";
-import { Center, ScrollView, Text, VStack } from "native-base";
-import React from "react";
+import { Center, ScrollView, Skeleton, Text, VStack } from "native-base";
+import React, { useState } from "react";
+
+const PHOTO_SIZE = 33;
 
 const Profile: React.FC = () => {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={{ uri: "https://github.com/luccahora.png" }}
-            alt="Foto do usuario"
-            size={33}
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor={"gray.500"}
+              endColor={"gray.400"}
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: "https://github.com/luccahora.png" }}
+              alt="Foto do usuario"
+              size={PHOTO_SIZE}
+            />
+          )}
         </Center>
       </ScrollView>
     </VStack>
