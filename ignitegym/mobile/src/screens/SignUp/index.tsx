11d1eca7447bbc1ee020d74abfa6bd52570089b3
Lucca Hora <luccahora@gmail.com>
2023-03-26 15:48:07 -0300

    chore: refactor with axios

--- ignitegym/mobile/src/screens/SignUp/index.tsx
+++ ignitegym/mobile/src/screens/SignUp/index.tsx
@@ -8,6 +8,7 @@ import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
 import Input from "@components/Input";
 import Button from "@components/Button";
+import { api } from "@services/api";
 
 type FormDataProps = {
   name: string;
@@ -43,16 +44,9 @@ const SignUp: React.FC = () => {
   };
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await fetch("http://192.168.1.4:3333/users", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name, email, password }),
-    });
-    const data = await response.json();
-    console.log(data);
+    const response = await api.post("/users", { name, email, password });
+
+    console.log(response.data);
   }
 
   return (
