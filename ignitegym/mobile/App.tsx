76eb70df0b1423f19e535517b5c5b19906319f6b
Lucca Hora <luccahora@gmail.com>
2023-04-23 15:45:32 -0300

    chore: Unificando o create e provider context

--- ignitegym/mobile/App.tsx
+++ ignitegym/mobile/App.tsx
@@ -9,7 +9,7 @@ import { Loading } from "@components/Loading";
 import { THEME } from "./src/theme";
 import React from "react";
 import { Routes } from "@routes/index";
-import { AuthContext } from "@contexts/AuthContext";
+import { AuthContextProvider } from "@contexts/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -20,18 +20,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <AuthContext.Provider
-        value={{
-          user: {
-            id: "1",
-            name: "Lucca",
-            email: "lucca@gmail.com",
-            avatar: "lucca.png",
-          },
-        }}
-      >
+      <AuthContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
